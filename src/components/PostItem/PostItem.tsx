40ae268d345142b0509e9a14f439d2218f634684
cashrina <khripunova.irina.24@gmail.com>
2024-06-17 20:22:44 +0600

    Добавлен интервал для получения сообщений

--- src/components/PostItem/PostItem.tsx
+++ src/components/PostItem/PostItem.tsx
@@ -5,29 +5,36 @@ const urlGet = "http://146.185.154.90:8000/messages";
 const PostItem = () => {
     const[posts, setPosts] = useState<Messages[]>([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch(urlGet);
+    const fetchData = async () => {
+        const response = await fetch(urlGet);
 
-            if (response.ok) {
-                const posts = await response.json() as Messages[];
-                const newPosts = posts.map(post => ({
-                    _id: post._id,
-                    message: post.message,
-                    author: post.author,
-                    datetime: post.datetime,
-                }));
-                setPosts(newPosts);
-            }
-        };
+        if (response.ok) {
+            const posts = await response.json() as Messages[];
+            const newPosts = posts.map(post => ({
+                _id: post._id,
+                message: post.message,
+                author: post.author,
+                datetime: post.datetime,
+            }));
+            setPosts(newPosts);
+        }
+    };
+
+    useEffect(() => {
         void fetchData();
+        setInterval(fetchData, 5000);
     }, []);
 
     console.log(posts);
 
     return (
-        <div>
-
+        <div className="card" style={{width: "18rem"}}>
+            {posts.map(post => (
+                <div key={post._id} className="card-body">
+                    <h5 className="card-title">{post.author}</h5>
+                    <p className="card-text">{post.message}</p>
+                </div>
+            ))}
         </div>
     );
 };
