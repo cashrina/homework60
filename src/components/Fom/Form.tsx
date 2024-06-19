042901dd6a5b887115beb92c3fe4183d63d6a062
cashrina <khripunova.irina.24@gmail.com>
2024-06-19 13:22:20 +0600

    Добавлена логика для отрисовки карточек через 3 секунды

--- src/components/Fom/Form.tsx
+++ src/components/Fom/Form.tsx
@@ -1,32 +1,39 @@
+
+import { FormState } from "../../types.ts";
 import React, {useState} from "react";
-import {FormState} from "../../types.ts";
 
 const Form = () => {
-    const [authorMessage, setAuthorMessage] = useState<FormState>({
-        message: '',
-        author: '',
+    const [formState, setFormState] = useState<FormState>({
+        message: "",
+        author: "",
     });
 
-    const changeMessage = (event:React.ChangeEvent<HTMLInputElement>) => {
-        setAuthorMessage((prev) => ({ ...prev,
-             [event.target.name]: event.target.value,
+    const changePost = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFormState((prev) => ({
+            ...prev,
+            [event.target.name]: event.target.value
         }));
     };
 
-    const onFormSubmit = async (event:React.FormEvent) => {
+    const onFormSubmit = async (event: React.FormEvent) => {
         event.preventDefault();
         try {
             const url = 'http://146.185.154.90:8000/messages';
             const data = new URLSearchParams();
-            data.set('message', authorMessage.message);
-            data.set('author', authorMessage.author);
-            console.log(authorMessage)
+            data.set('message', formState.message);
+            data.set('author', formState.author);
+
             await fetch(url, {
                 method: 'post',
                 body: data,
             });
-        } catch (e) {
-            console.error(e);
+            setFormState({
+                message: "",
+                author: "",
+            });
+
+        } catch (error) {
+            console.error('Error submitting form:', error);
         }
     };
 
@@ -34,22 +41,29 @@ const Form = () => {
         <form onSubmit={onFormSubmit}>
             <div className="mb-3">
                 <label className="form-label">Admin</label>
-                <input required
-                       style={{cursor: "pointer"}}
-                       type="text"
-                       className="form-control"
-                       aria-describedby="Write your name please"
-                       name="author" onChange={changeMessage}/>
+                <input
+                    required
+                    style={{ cursor: "pointer" }}
+                    type="text"
+                    className="form-control"
+                    aria-describedby="Write your name please"
+                    name="author"
+                    value={formState.author}
+                    onChange={changePost}
+                />
                 <div className="form-text">Write your name please.</div>
             </div>
             <div className="mb-3">
                 <label className="form-label">Message</label>
-                <input required
-                       style={{cursor: "pointer"}}
-                       type="text"
-                       className="form-control"
-                       name="message"
-                       onChange={changeMessage}/>
+                <input
+                    required
+                    style={{ cursor: "pointer" }}
+                    type="text"
+                    className="form-control"
+                    name="message"
+                    value={formState.message}
+                    onChange={changePost}
+                />
                 <div className="form-text">Write message please.</div>
             </div>
             <button type="submit" className="btn btn-outline-primary">Submit</button>
--- src/components/PostItem/PostItem.tsx
+++ src/components/PostItem/PostItem.tsx
@@ -1,42 +1,51 @@
-import {useEffect, useState} from "react";
-import {Messages} from "../../types.ts";
+import { useEffect, useState } from "react";
+import { Messages } from "../../types.ts";
 import SharkImage from "../../assets/shark.png";
 
 const urlGet = "http://146.185.154.90:8000/messages";
 
 const PostItem = () => {
-    const[posts, setPosts] = useState<Messages[]>([]);
+    const [posts, setPosts] = useState<Messages[]>([]);
 
     const fetchData = async () => {
-        const response = await fetch(urlGet);
-
-        if (response.ok) {
-            const posts = await response.json() as Messages[];
-            const newPosts = posts.map(post => ({
-                _id: post._id,
-                message: post.message,
-                author: post.author,
-                datetime: post.datetime,
-            }));
-            setPosts(newPosts);
+        try {
+            const response = await fetch(urlGet);
+            if (response.ok) {
+                const newPosts = await response.json() as Messages[];
+                const filteredNewPosts = newPosts.filter(newPost =>
+                    !posts.find(post => post._id === newPost._id)
+                );
+                if (filteredNewPosts.length > 0) {
+                    const updatedPosts = [...posts, ...filteredNewPosts];
+                    updatedPosts.sort((a, b) => new Date(b.datetime).getTime() - new Date(a.datetime).getTime());
+                    setPosts(updatedPosts);
+                }
+            }
+        } catch (error) {
+            console.error("Error fetching messages:", error);
         }
     };
 
     useEffect(() => {
         void fetchData();
-        setInterval(fetchData, 5000);
-    }, []);
 
-    console.log(posts);
+        const interval = setInterval(fetchData, 3000);
+
+        return () => clearInterval(interval);
+    }, [posts]);
 
     const dateFormat = (date: string): string => {
-        let d:Date = new Date(date);
-        return [d.getMonth() + 1,
+        let d: Date = new Date(date);
+        return [
+                d.getMonth() + 1,
                 d.getDate(),
-                d.getFullYear()].join('/') + ' ' +
-            [d.getHours(),
+                d.getFullYear(),
+            ].join('/') + ' ' +
+            [
+                d.getHours(),
                 d.getMinutes(),
-                d.getSeconds()].join(':');
+                d.getSeconds(),
+            ].join(':');
     };
 
     return (
@@ -45,9 +54,9 @@ const PostItem = () => {
                 <div key={post._id} className="col-md-6 mb-4 mt-3">
                     <div className="card border border-primary shadow p-3 mb-5 bg-body-tertiary">
                         <div className="card-body">
-                            <div className="d-flex mb-3 col-md-6">
+                            <div className="d-flex mb-3">
                                 <img alt="icon" src={SharkImage}
-                                     style={{height: '80px', width: '80px', marginRight: '10px'}} className="me-5"/>
+                                     style={{ height: '80px', width: '80px', marginRight: '10px' }} className="me-5" />
                                 <div className="d-flex justify-content-between">
                                     <h5 className="card-title text-primary mb-0 me-5 ms-5">{post.author}</h5>
                                     <p className="card-text text-muted mb-0 ms-5">{dateFormat(post.datetime)}</p>
